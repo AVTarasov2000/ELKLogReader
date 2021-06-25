3cfba1804398c73bcc4c1f0b8776cc16e8c7fa50
avtarasov <tarasov_av@cs.vsu.ru>
2021-06-25 17:08:21 +0300

    ready to work

--- app/controller.py
+++ app/controller.py
@@ -1,16 +1,11 @@
 # from waitress import serve
 from flask import Flask, request, make_response, render_template
 from flask_cors import CORS
-from app.service import search_by_timestamp_and_msg_id, get_all_fields_to_search, search_by_args
+from app.service import search_by_timestamp_and_msg_id, get_all_fields_to_search, search_by_args, difference_by_id, delete_index
 from app.Node import Node
 
 app = Flask(__name__)
 CORS(app)
-
-def make_resp(message, status):
-    resp = make_response(message, status)
-    resp.headers['Content-type'] = 'application/json; charset=utf-8'
-    return resp
 
 
 @app.route("/")
@@ -41,6 +36,19 @@ def search():
     return {"result": res, "count": len(result['hits']['hits'])}
 
 
+@app.route("/search/difference", methods=['POST'])
+def difference():
+    data = request.json
+    res = difference_by_id(data['file1'], data['file2'], data['field'])
+    return {"result": res, "count": len(res)}
+
+
+@app.route("/index/delete", methods=['GET'])
+def del_index():
+    delete_index()
+    return {}
+
+
 @app.route("/getTree", methods=['GET'])
 def get_tree():
     result, name = get_all_fields_to_search()
@@ -64,9 +72,3 @@ def get_tree():
             else:
                 node.items.append(Node(n, [], node.path+"."+n, node.content[n]['type']))
     return root.get_json()
-
-
-#
-#
-# if __name__ == '__main__':
-#     serve(app, host='0.0.0.0', port='5000')
--- app/service.py
+++ app/service.py
@@ -1,12 +1,12 @@
 import datetime
 import os
 from elasticsearch import Elasticsearch
-# es = Elasticsearch(os.environ.get('ELASTICSEARCH_URL'))
+# es = Elasticsearch("http://localhost:9200")
 es = Elasticsearch("http://elasticsearch:9200")
 
 es_info = Elasticsearch.info(es)
 
-index = "test_for_logreader"
+index = "logreader"
 
 
 def search_by_msg_id(msg_id: str):
@@ -24,52 +24,60 @@ def search_by_timestamp_and_msg_id(start_date: str, end_date:str, msg_id:str):
         end_date = "*"
     if not start_date:
         start_date = "*"
-    return es.search(index=index, body={"size":1000 ,"query": {"query_string": {
+    return es.search(index=index, body={"size":10000 ,"query": {"query_string": {
     "query": f"@timestamp:[{start_date} TO {end_date}] AND MsgId:\"{msg_id}\""}}})
 
 
 def get_all_fields_to_search():
-    return es.indices.get_mapping(index=index)[index]['mappings']['properties'], index
+    es.indices.create(index=index, ignore=400)
+    res = es.indices.get_mapping(index=index)[index]['mappings']
+    if 'properties' in res:
+        return res['properties'], index
+    else:
+        return [], index
 
 
 def search_by_args(queries):
     if len(queries) <= 0:
-        return es.search(index=index, body={"size":1000 ,"query": {"match_all" :{}}})
+        return es.search(index=index, body={"size":10000 ,"query": {"match_all" :{}}})
     else:
         query = queries[0]
         for q in queries[1:]:
             query += f"AND {q}"
-        return es.search(index=index, body={"size":1000 ,"query": {"query_string": {
+        return es.search(index=index, body={"size":10000 ,"query": {"query_string": {
                 "query": query}}})
 
 
 def difference_by_id(file1, file2, field_path):
     res = []
     for id in es.search(index=index, body={"size": 10000,  "fields": [field_path], "_source": False})['hits']['hits']:
-        message = ""
-        var1 = es.search(index=index,
-                        body={"query": {"match": {
-                            field_path: f"{id['fields'][field_path]}",
-                            "path": file1
-                        }}
-                        ,"fields": ['log_timestamp'], "_source": False})['hits']['hits']
-        var2 = es.search(index=index,
-                         body={"query": {"match": {
-                             field_path: f"{id['fields'][field_path]}",
-                             "path": file2
-                         }}
-                             , "fields": ['log_timestamp'], "_source": False})['hits']['hits']
+        message = []
+        var1 = es.search(index=index,body = {"size": 1000,
+                                             "query": {
+                                                 "query_string": {
+                                                    "query": f"{field_path}: \"{id['fields'][field_path][0]}\" AND path: \"{file1}\""
+                                                 }
+                                             }
+                                            ,"fields": ['log_timestamp'], "_source": False})['hits']['hits']
+        var2 = es.search(index=index,body = {"size": 1000,
+                                             "query": {
+                                                 "query_string": {
+                                                    "query": f"{field_path}: \"{id['fields'][field_path][0]}\" AND path: \"{file2}\""
+                                                 }
+                                             }
+                                            ,"fields": ['log_timestamp'], "_source": False})['hits']['hits']
         if var1 and var2:
-            message += str(id['fields'][field_path][0])+"\n"
-            message += f"{var1['fields']['log_timestamp'][0]} - {var2['fields']['log_timestamp'][0]} = "
-            message += str((datetime.datetime.strptime(var1['fields']['log_timestamp'][0], "%Y-%m-%d %H:%M:%S.%f")
-                           - datetime.datetime.strptime(var2['fields']['log_timestamp'][0], "%Y-%m-%d %H:%M:%S.%f")).microseconds)
-            message += "\n\n"
-        res.append({"message":message, "path": str(id['fields'][field_path][0])})
+            message.append(str(id['fields'][field_path][0]))
+            message.append(f"{var1[0]['fields']['log_timestamp'][0]} - {var2[0]['fields']['log_timestamp'][0]} = " +
+                           str((datetime.datetime.strptime(var1[0]['fields']['log_timestamp'][0], "%Y-%m-%d %H:%M:%S.%f")
+                           - datetime.datetime.strptime(var2[0]['fields']['log_timestamp'][0], "%Y-%m-%d %H:%M:%S.%f")).microseconds))
+        res.append({"message":message, "path": f"{file1} - {file2}"})
+    return res
 
 
 def delete_index():
     es.indices.delete(index=index, ignore=[400, 404])
+    es.indices.create(index=index, ignore=400)
 
 # difference_by_id()
 # print(es.search(index=index, body={"query": {"match": {"params.RequestId": "X'50400000000000022fc75e9ebe5611eb850bac100e1f0000'"}}}))
